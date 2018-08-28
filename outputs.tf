71d2942a9a39130d1489f625dc2e1e11d3eede27
Glenn Bech <glenn.bech@gmail.com>
2018-08-28 17:27:10 +0200

    even more tweaking

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,7 @@ output "arn" {
   description = "The ARN of the load balancer."
   value       = "${element(concat(aws_lb.main.*.arn, aws_lb.main_with_access_logs.*.arn), 0)}"}
 
+////
 output "name" {
   description = "The name of the load balancer."
   value       = "${element(split("/", element(concat(aws_lb.main.*.name, aws_lb.main_with_access_logs.*.name), 0)),2)}"
@@ -12,12 +13,12 @@ output "name" {
 
 output "dns_name" {
   description = "The DNS name of the load balancer."
-  value       = "${element(split("/", element(concat(aws_lb.main.*.dns_name, aws_lb.main_with_access_logs.*.dns_name), 0)),2)}"
+  value       = "${element(concat(aws_lb.main.*.dns_name, aws_lb.main_with_access_logs.*.dns_name),0)}"
 }
 
 output "zone_id" {
   description = "The canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
-  value       = "${element(split("/", element(concat(aws_lb.main.*.zone_id, aws_lb.main_with_access_logs.*.zone_id), 0)),2)}"
+  value       = "${element(concat(aws_lb.main.*.zone_id, aws_lb.main_with_access_logs.*.zone_id), 0)}"
 }
 
 output "origin_id" {
