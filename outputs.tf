b102efd1fce21280c2c4303d3c448be189ade1ff
Glenn Bech <glenn.bech@gmail.com>
2018-08-29 08:39:05 +0200

    Rewrite from element(concat()) to coalesce(join())

--- outputs.tf
+++ outputs.tf
@@ -3,27 +3,27 @@
 # ------------------------------------------------------------------------------
 output "arn" {
   description = "The ARN of the load balancer."
-  value       = "${element(concat(aws_lb.main.*.arn, aws_lb.main_with_access_logs.*.arn), 0)}"
+  value       = "${coalesce(join("",aws_lb.main.*.arn), join("", aws_lb.main_with_access_logs.*.arn))}"
 }
 
 output "name" {
   description = "The name of the load balancer."
-  value       = "${element(split("/", element(concat(aws_lb.main.*.name, aws_lb.main_with_access_logs.*.name), 0)),2)}"
+  value       = "${element(split("/", coalesce(join("",aws_lb.main.*.name),  join("", aws_lb.main_with_access_logs.*.name))),2)}"
 }
 
 output "dns_name" {
   description = "The DNS name of the load balancer."
-  value       = "${element(concat(aws_lb.main.*.dns_name, aws_lb.main_with_access_logs.*.dns_name),0)}"
+  value       = "${coalesce(join("",aws_lb.main.*.dns_name), join("", aws_lb.main_with_access_logs.*.dns_name))}"
 }
 
 output "zone_id" {
   description = "The canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
-  value       = "${element(concat(aws_lb.main.*.zone_id, aws_lb.main_with_access_logs.*.zone_id), 0)}"
+  value       = "${coalesce(join("",aws_lb.main.*.zone_id), join("", aws_lb.main_with_access_logs.*.zone_id))}"
 }
 
 output "origin_id" {
   description = "First part of the DNS name of the load balancer."
-  value       = "${element(split(".", element(concat(aws_lb.main.*.dns_name, aws_lb.main_with_access_logs.*.dns_name), 0)),0)}"
+  value       = "${element(split(".", coalesce(join("",aws_lb.main.*.dns_name), join("", aws_lb.main_with_access_logs.*.dns_name))),0)}"
 }
 
 output "security_group_id" {
