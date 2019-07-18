007344d179036c88c7f7c3a522f314412c81c12a
itsdalmo <kristian@doingit.no>
2019-07-18 23:41:28 +0200

    Fix output errors for tf destroy

--- outputs.tf
+++ outputs.tf
@@ -3,27 +3,27 @@
 # ------------------------------------------------------------------------------
 output "arn" {
   description = "The ARN of the load balancer."
-  value       = concat(aws_lb.main[*].arn, aws_lb.main_with_access_logs[*].arn)[0]
+  value       = concat(aws_lb.main[*].arn, aws_lb.main_with_access_logs[*].arn, [""])[0]
 }
 
 output "name" {
   description = "The name of the load balancer."
-  value       = element(split("/", concat(aws_lb.main[*].name, aws_lb.main_with_access_logs[*].name)[0]), 2)
+  value       = element(split("/", concat(aws_lb.main[*].name, aws_lb.main_with_access_logs[*].name, [""])[0]), 2)
 }
 
 output "dns_name" {
   description = "The DNS name of the load balancer."
-  value       = concat(aws_lb.main[*].dns_name, aws_lb.main_with_access_logs[*].dns_name)[0]
+  value       = concat(aws_lb.main[*].dns_name, aws_lb.main_with_access_logs[*].dns_name, [""])[0]
 }
 
 output "zone_id" {
   description = "The canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
-  value       = concat(aws_lb.main[*].zone_id, aws_lb.main_with_access_logs[*].zone_id)[0]
+  value       = concat(aws_lb.main[*].zone_id, aws_lb.main_with_access_logs[*].zone_id, [""])[0]
 }
 
 output "origin_id" {
   description = "First part of the DNS name of the load balancer."
-  value       = element(split(".", concat(aws_lb.main[*].dns_name, aws_lb.main_with_access_logs[*].dns_name)[0]), 0)
+  value       = element(split(".", concat(aws_lb.main[*].dns_name, aws_lb.main_with_access_logs[*].dns_name, [""])[0]), 0)
 }
 
 output "security_group_id" {
